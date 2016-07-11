6ae591421cdd2c3322c144915591a787afc5dbee
VENKATA SIVAKUMAR GANDIKOTA <gsivakumar.608@gmail.com>
2016-07-11 14:35:38 -0700

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [title] VARCHAR(50) NOT NULL, 
+    [releaseyear] INT NOT NULL, 
+    [language] VARCHAR(50) NOT NULL, 
+    [length] FLOAT NOT NULL
+)
